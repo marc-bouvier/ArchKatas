115a3c37b695de31f2297ea5025ccc80c2c371c9
Marc Bouvier <marc@pop-os.localdomain>
2021-04-02 15:23:01 +0200

    html page and deno unit test

---
+++ test/load_kata_test.js
@@ -0,0 +1,7 @@
+import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts";
+// import { urlsToMarkdownList } from "../shared/lib/formatter.js";
+
+Deno.test("assert test framework is working", () => {
+  assertEquals(3,3);
+});
+
